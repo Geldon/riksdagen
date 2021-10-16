9376565049243b98344981faaf768d9b655af4fd
arbetsformedlingen_garcn <christian.gardebrink@arbetsformedlingen.se>
2021-10-16 11:50:14 +0200

    partibeteckning

---
+++ src/models/partibeteckning.enum.ts
@@ -0,0 +1,10 @@
+export enum Partibeteckning {
+  Socialdemokraterna = 'S',
+  Kristdemokraterna = 'KD',
+  Centerpartiet = 'C',
+  Moderaterna = 'M',
+  Miljopartiet = 'MP',
+  Sverigedemokraterna = 'SD',
+  Vansterpartiet = 'V',
+  Liberalerna = 'L',
+}
